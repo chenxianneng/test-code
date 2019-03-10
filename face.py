7fddf3c9151800dff016fd4afeafa2dd5e3e7f02
chenxianneng <31298910+chenxianneng@users.noreply.github.com>
2019-03-10 13:32:30 +0800

    人脸识别

---
+++ face.py
@@ -0,0 +1,67 @@
+import face_recognition
+import time
+import os
+from os.path import basename
+import socketserver
+
+girls_info = []
+#girl = {'name':'吉泽明步', 'face_encoding':'', 'base_info':'已故女优'}
+count = 0
+
+dst_path = 'f:/test-code/img/'
+for file in os.listdir(dst_path):
+    #print(file)
+
+    girl = {}
+    girl['name'] = basename(file)
+    print(girl['name'])
+    
+    picture_of_me = face_recognition.load_image_file(dst_path + file)
+    result = face_recognition.face_encodings(picture_of_me)
+    if len(result) == 0:
+        print('未识别到图片', file)
+        continue
+    
+    girl['face_encoding'] = result[0]
+    girls_info.append(girl)
+    count += 1
+    #print(count)
+
+
+def get_girl_name(path):
+    unknown_picture = face_recognition.load_image_file(path)
+    result = face_recognition.face_encodings(unknown_picture)
+    if len(result) == 0:
+        print('未识别到你的图片', path)
+        return '未识别到你的图片'
+
+    unknown_face_encoding = result[0]
+    for girl in girls_info:
+        results = face_recognition.compare_faces([girl['face_encoding']], unknown_face_encoding)
+        if results[0] == True:
+            #print(girl['name'])
+            return girl['name']
+
+    return '没有找到这个女优'
+
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        #print(self.data)
+
+        path = 'f:/test-code/' + self.data.decode('utf-8')
+        print(path)
+        
+        result = get_girl_name(path)
+        print(result)
+        self.request.sendall(bytes(result, 'utf-8'))
+
+        
+HOST, PORT = "localhost", 9999
+#server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+server = socketserver.ThreadingTCPServer(( 'localhost', 9999), MyTCPHandler)
+server.serve_forever()
