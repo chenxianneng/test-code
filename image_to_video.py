5c2920517a59197f4d573e69d5612ee29363da03
chenxianneng <cxn8801@gmail.com>
2019-04-04 16:38:33 +0800

    fix bug

--- image_to_video.py
+++ image_to_video.py
@@ -3,8 +3,8 @@ import os.path
 import glob
 
 def make_square(img, fill_color=(0, 0, 0, 0)):
-    screen_width = 1366
-    screen_height = 768
+    screen_width = 1366.0
+    screen_height = 768.0
 
     x, y = img.size
     new_x, new_y = 0, 0
@@ -17,7 +17,7 @@ def make_square(img, fill_color=(0, 0, 0, 0)):
 
     print(x, y, new_x, new_y)
     img = img.resize((int(new_x), int(new_y)),Image.BILINEAR)   
-    new_im = Image.new('RGBA', (screen_width, screen_height), fill_color)
+    new_im = Image.new('RGBA', (int(screen_width), int(screen_height)), fill_color)
     pos_x, pos_y = (screen_width - new_x) / 2, (screen_height - new_y) / 2
     new_im.paste(img, (int(pos_x), int(pos_y)))
     return new_im
@@ -39,10 +39,16 @@ from moviepy.editor import *
 from moviepy.audio.fx import all
 
 def run(directory, back_ground_music, out_dir):
+    if os.path.isdir(out_dir):
+        import shutil
+        shutil.rmtree(out_dir)
+    os.makedirs(out_dir)
+
     base_dir = os.path.realpath(directory)
     print(base_dir)
 
-    for jpgfile in glob.glob(base_dir + "*.jpg"):
+    for jpgfile in glob.glob(directory + "*.jpg"):
+        print(jpgfile)
         convertjpg(jpgfile, out_dir)
     
     fps = 24
@@ -56,4 +62,4 @@ def run(directory, back_ground_music, out_dir):
     concat_clip = concatenate_videoclips(clips, method="compose")
     concat_clip.write_videofile(out_dir + "output.mp4", fps=fps, audio=back_ground_music)
 
-run('f:/test-code/youtube/input/', 'f:/test-code/youtube/back_ground.mp3', 'f:/test-code/youtube/output/')
+run('D:/development/test-coder/youtube/input/', 'd:/development/test-coder/youtube/back_ground.mp3', 'D:/development/test-coder/youtube/output/')
